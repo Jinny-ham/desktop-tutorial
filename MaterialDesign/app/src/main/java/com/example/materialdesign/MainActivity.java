3fc6d7696ad8396c186e5c37555a8782431d64ed
Jinny-ham <flxmfzos1@gmail.com>
2020-06-03 12:36:13 +0900

    Standalone Toolbar

--- MaterialDesign/app/src/main/java/com/example/materialdesign/MainActivity.java
+++ MaterialDesign/app/src/main/java/com/example/materialdesign/MainActivity.java
@@ -19,51 +19,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        getSupportActionBar().setTitle("Hello!");
+        toolbar.setTitle("Welcome!");
         toolbar.setSubtitle("Folks!");
 
 //        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
 //            toolbar.setElevation(10f);
 //        }
+//
+//        toolbar.setLogo(R.drawable.raon);
+//        toolbar.setNavigationIcon(R.drawable.suri);
+        toolbar.inflateMenu(R.menu.menu_main);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                String msg = "";
 
-        //.setLogo(R.drawable.);
-        //toolbar.setNavigationIcon();
+                switch (item.getItemId()){
+                    case R.id.discard:
+                        msg = "Delete";
+                        break;
+                    case R.id.search:
+                        msg = "Search";
+                        break;
+                    case R.id.edit:
+                        msg = "Edit";
+                        break;
+                    case R.id.settings:
+                        msg = "Settings";
+                        break;
+                    case R.id.exit:
+                        msg = "Exit";
+                        break;
+                }
+
+                Toast.makeText(MainActivity.this, msg + " clicked!", Toast.LENGTH_SHORT).show();
+
+                return true;
+            }
+        });
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        String msg = "";
-
-        switch (item.getItemId()){
-            case R.id.discard:
-                msg = "Delete";
-                break;
-            case R.id.search:
-                msg = "Search";
-                break;
-            case R.id.edit:
-                msg = "Edit";
-                break;
-            case R.id.settings:
-                msg = "Settings";
-                break;
-            case R.id.exit:
-                msg = "Exit";
-                break;
-        }
-
-        Toast.makeText(this, msg + " clicked!", Toast.LENGTH_SHORT).show();
-
-        return super.onOptionsItemSelected(item);
-    }
-
 
 }
